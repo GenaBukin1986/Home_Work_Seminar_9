352f6fbe11bc8e62fc4df30ac28a3c45cf919e48
Nick_Bear <bear_76@mail.ru>
2023-07-08 14:23:21 +0300

    Домашняя работа семинара № 9

---
+++ task01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+using System;
+using static System.Console;
+
+string PrintNumber(int n)
+{
+    if (n == 1) return "1";
+    return n.ToString() + ", " + PrintNumber(n - 1);
+}
+
+Clear();
+Write("Введите число: ");
+int nums = int.Parse(ReadLine()!);
+WriteLine(PrintNumber(nums));
---
+++ task02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+using System;
+using static System.Console;
+
+int SumNumbers(int m, int n)
+{
+    if (m == n) return n;
+    return m + SumNumbers(m + 1, n);
+}
+
+Clear();
+Write("Введите первое число: ");
+int num1 = int.Parse(ReadLine()!);
+Write("Введите второе число: ");
+int num2 = int.Parse(ReadLine()!);
+WriteLine($"Сумма натуральных элементов в промежутке от {num1} до {num2} равна {SumNumbers(num1, num2)}");
